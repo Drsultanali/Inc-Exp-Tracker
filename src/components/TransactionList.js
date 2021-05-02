afd02cf59cce0d1f66d66507950bfe859b6dc144
Sultan Ali <66969779+Drsultanali@users.noreply.github.com>
2021-05-02 16:50:15 +0500

    Update TransactionList.js

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,17 +1,19 @@
-import React, { useContext } from 'react';
-import { Transaction } from './Transaction';
+import { useContext } from "react";
+import Transaction from "./Transaction";
+import { GlobalContext } from "../context/GlobalState";
 
-import { GlobalContext } from '../context/GlobalState';
-
-export const TransactionList = () => {
+const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
-
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
       </ul>
     </>
-  )
-}
+  );
+};
+
+export default TransactionList;
