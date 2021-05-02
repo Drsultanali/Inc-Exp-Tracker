1a067e949f1c1baac96e2973d669263a5e35d7e5
Sultan Ali <66969779+Drsultanali@users.noreply.github.com>
2021-05-02 16:48:44 +0500

    Update Balance.js

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,33 +1,18 @@
-import React, { useContext } from 'react';
-import { GlobalContext } from '../context/GlobalState';
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
-//Money formatter function
-function moneyFormatter(num) {
-  let p = num.toFixed(2).split('.');
-  return (
-    '$ ' +
-    p[0]
-      .split('')
-      .reverse()
-      .reduce(function (acc, num, i, orig) {
-        return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;
-      }, '') +
-    '.' +
-    p[1]
-  );
-}
-
-export const Balance = () => {
+const Balance = () => {
   const { transactions } = useContext(GlobalContext);
 
-  const amounts = transactions.map(transaction => transaction.amount);
-
-  const total = amounts.reduce((acc, item) => (acc += item), 0);
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
   return (
     <>
-      <h4>Your Balance</h4>
-    <h1>{moneyFormatter(total)}</h1>
+      <h4 className="text-center">Your Balance</h4>
+      <h1 className="text-center">${total}</h1>
     </>
-  )
-}
+  );
+};
+
+export default Balance;
