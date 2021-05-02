e821e7cf16312a92087b945c48c38b9002b228a5
Sultan Ali <66969779+Drsultanali@users.noreply.github.com>
2021-05-02 16:49:08 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ import React from 'react'
 export const Header = () => {
     return (
         <h2>
-            Expense Tracker
+            Expense Tracker By Sultan Ali
         </h2>
     )
 }
+export default Header;
